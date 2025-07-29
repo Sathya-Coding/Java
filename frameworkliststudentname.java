30248e291b17768bdd70878be4349ecb0551a909
Sathya <psathyapriya2002@gmail.com>
2025-07-29 11:48:54 +0530

    Add files via upload

---
+++ frameworkliststudentname.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class frameworkliststudentname {
+    public static void main(String[]args){
+        List<String>students=new ArrayList<>();
+        students.add("Sathya");
+        students.add("Manikandan");
+        students.add("Magizhini");
+        students.add("Sathya");
+        students.add("Sumithra");
+        System.out.println("Students name:"+students);
+    }
+}
