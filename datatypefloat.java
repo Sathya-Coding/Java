60f27f9ea53c596210a0db1ed83df72c6d33cbcf
Sathya <psathyapriya2002@gmail.com>
2025-08-01 10:25:17 +0530

    Add files via upload

---
+++ datatypefloat.java
@@ -0,0 +1,8 @@
+public class datatypefloat {
+        public static void main(String[] args) {
+            float milkLiters = 2.5f;
+            System.out.println("Milk bought today: " + milkLiters + " liters");
+        }
+    }
+
+
