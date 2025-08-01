8a26f2e6092591981440863d815c5f779e7bb23a
Sathya <psathyapriya2002@gmail.com>
2025-08-01 10:24:35 +0530

    Add files via upload

---
+++ datatypedouble.java
@@ -0,0 +1,10 @@
+public class datatypedouble {
+        public static void main(String[] args) {
+            double total = 345.75;
+            double tax = total * 0.05;
+            double finalAmount = total + tax;
+            System.out.println("Final bill amount with tax: ₹" + finalAmount);
+        }
+    }
+
+
