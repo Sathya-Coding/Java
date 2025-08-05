aa7c5c49ca781967075cf873b7bb9d6fb8036bf6
Sathya <psathyapriya2002@gmail.com>
2025-08-05 11:23:51 +0530

    Add files via upload

---
+++ ifmovieticket.java
@@ -0,0 +1,10 @@
+public class ifmovieticket {
+        public static void main(String[] args) {
+            int age = 10;
+            if (age <= 12) {
+                System.out.println("Eligible for child discount on movie ticket.");
+            }
+        }
+    }
+
+
