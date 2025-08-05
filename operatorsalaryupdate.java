7e6ddc58f11265ec86caeef103b4864ff8145ce2
Sathya <psathyapriya2002@gmail.com>
2025-08-05 11:22:52 +0530

    Add files via upload

---
+++ operatorsalaryupdate.java
@@ -0,0 +1,10 @@
+public class operatorsalaryupdate {
+        public static void main(String[] args) {
+            int salary = 25000;
+            int bonus = 5000;
+            salary += bonus;
+            System.out.println("Updated Salary: ₹" + salary);
+        }
+    }
+
+
