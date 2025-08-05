164c8b0996076137fc16bccacb2f107fda773fc8
Sathya <psathyapriya2002@gmail.com>
2025-08-05 11:21:58 +0530

    Add files via upload

---
+++ operatorarithmetichotelbill.java
@@ -0,0 +1,10 @@
+public class operatorarithmetichotelbill {
+        public static void main(String[] args) {
+            int totalBill = 1200;
+            int friends = 4;
+            int eachPay = totalBill / friends;
+            System.out.println("Each person pays: ₹" + eachPay);
+        }
+    }
+
+
