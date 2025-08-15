498a5665efa8b1c06fb85cce70106c59d8f07c26
Sathya <psathyapriya2002@gmail.com>
2025-08-15 18:46:50 +0530

    Add files via upload

---
+++ frameworkhashset.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class frameworkhashset {
+    public static void main(String[]args){
+        Set<String>fruits=new HashSet<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Apple");
+        System.out.println("Fruits:"+fruits);
+
+    }
+}
