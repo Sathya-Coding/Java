8f197847b56cb35dc5b63b0ad27bacf5174c08b9
Sathya <psathyapriya2002@gmail.com>
2025-08-16 15:54:32 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    // Simple Java Program
+
+        public static void main(String[] args) {
+            // Print message to the console
+            System.out.println("Hello, World!");
+        }
+    }
+
+
