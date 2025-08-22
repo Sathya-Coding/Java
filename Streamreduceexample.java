6841dfbcb36b2c50456402bdc8893b7be03f90e9
Sathya <psathyapriya2002@gmail.com>
2025-08-22 13:10:53 +0530

    Add files via upload

---
+++ Streamreduceexample.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Streamreduceexample {
+    public static void main(String[]args){
+        List<Integer>numbers= Arrays.asList(10,20,30,40);
+        int sum=numbers.stream().reduce(0,(a,b)->a+b);
+        System.out.println("Sum="+sum);
+    }
+}
